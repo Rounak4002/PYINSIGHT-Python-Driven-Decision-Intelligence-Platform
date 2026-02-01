5d2e3871f852a6dcf1fbb1a0b66d07ea6231aaf0
ROUNAK PATTANAIK <pattanaikrounak182@gmail.com>
2026-02-01 22:24:21 +0530

    Create api_data_fetcher.py

---
+++ realtime/api_data_fetcher.py
@@ -0,0 +1,11 @@
+import requests
+import pandas as pd
+
+url = "https://api.exchangerate-api.com/v4/latest/USD"
+response = requests.get(url)
+data = response.json()
+
+df = pd.DataFrame(data['rates'].items(), columns=['currency', 'rate'])
+df.to_csv('realtime_data.csv', index=False)
+
+print("Real-time data fetched.")
